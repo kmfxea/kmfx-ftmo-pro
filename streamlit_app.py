df271524e61f2d5693b9b982911c8f4b5e785eed
kmfxea <kmfxea@gmail.com>
2026-02-10 12:49:58 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -874,10 +874,10 @@ End of 2025: Pioneer community formed — mga believers na sumali at naging part
     col1, col2 = st.columns(2)
     with col1:
         img1 = make_same_size("assets/ftmo.jpeg", target_width=800, target_height=700)
-        st.image(img1, use_container_width=True, caption="Passed Phase 1 in 13 days! 🎉")
+        #st.image(img1, use_container_width=True, caption="Passed Phase 1 in 13 days! 🎉")
     with col2:
         img2 = make_same_size("assets/ongoing.jpg", target_width=800, target_height=700)
-        st.image(img2, use_container_width=True, caption="Current challenge - full trust mode 🚀")
+        #st.image(img2, use_container_width=True, caption="Current challenge - full trust mode 🚀")
 
     st.write("""
 **First Taste of Pro Validation – Then the Hard Reset**
